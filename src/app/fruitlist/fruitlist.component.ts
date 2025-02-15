8b42b18469ce1623189363c331c8aa3ce3df9907
Albina Januzi <albina_januzi@hotmail.com>
2025-02-15 23:36:20 +0100

    Comment Name & Text If Comment Box Empty, than put "No Comment" text on it

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fruitlist',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
